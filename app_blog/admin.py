5427d5d4e86e67bcc16d3cf81f34b930065a7224
Roma <topgame324@gmail.com>
2024-06-05 12:18:13 +0300

    PW-3

--- app_blog/admin.py
+++ app_blog/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from .models import BlogPost
+from .models import Article, ArticleImage, Category
 
-admin.site.register(BlogPost)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category', 'slug')
+
+class ArticleImageInline(admin.TabularInline):
+    model = ArticleImage
+    extra = 1
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'main_page')
+    list_filter = ['pub_date', 'main_page']
+    search_fields = ['title', 'description']
+    inlines = [ArticleImageInline]
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
